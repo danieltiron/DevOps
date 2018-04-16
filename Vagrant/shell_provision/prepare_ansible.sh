ae0aabc258e431214bd5bf4fe85a152fcdf9a661
Daniel Tiron <daniel.tiron@gmail.com>
2018-04-16 14:16:54 +0300

    users and plugins automation

--- Vagrant/shell_provision/prepare_ansible.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo apt-get update
-sudo apt-get install software-properties-common
-sudo apt-add-repository ppa:ansible/ansible
-sudo apt-get update
-sudo apt-get install -y ansible
-sudo cp -a ansible-scripts/ssh_keys/. /home/vagrant/.ssh/
-sudo chmod 600 /home/vagrant/.ssh/id_rsa.pub
-sudo chmod 600 /home/vagrant/.ssh/id_rsa
--- Vagrant/shell_provision/prepare_jenkins.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo cat /tmp/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
-sudo rm /tmp/id_rsa.pub
